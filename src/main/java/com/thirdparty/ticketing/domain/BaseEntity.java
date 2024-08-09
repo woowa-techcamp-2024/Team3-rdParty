dab5fa6f1ac3dbac4fb3f2b3dea5a394316340f0
hseong3243 <hseong3243@gmail.com>
2024-08-09 21:50:53 +0900

    feat: `BaseEnttiy` 구현
    생성일, 생성자 정보를 가지고 있는 `BaseEntity`를 구현함.

---
+++ src/main/java/com/thirdparty/ticketing/domain/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.thirdparty.ticketing.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.ZonedDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private ZonedDateTime createdAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+}
