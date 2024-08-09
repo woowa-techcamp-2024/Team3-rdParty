5b8dfc0b4ee5391844fdfcb438454e2148714820
hseong3243 <hseong3243@gmail.com>
2024-08-09 15:45:31 +0900

    프로젝트 초기화

---
+++ src/test/java/com/thirdparty/ticketing/TicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thirdparty.ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
