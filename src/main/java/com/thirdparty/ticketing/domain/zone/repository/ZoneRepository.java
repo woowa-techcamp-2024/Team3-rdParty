51e35ec7c329885e75580c36ee8560edef4ec0b1
lass9436 <lass9436@gmail.com>
2024-08-13 01:52:25 +0900

    feat: 레포지토리에서 구역을 공연으로 조회하는 메서드 작성

--- src/main/java/com/thirdparty/ticketing/domain/zone/repository/ZoneRepository.java
+++ src/main/java/com/thirdparty/ticketing/domain/zone/repository/ZoneRepository.java
@@ -1,7 +1,12 @@
 package com.thirdparty.ticketing.domain.zone.repository;
 
+import com.thirdparty.ticketing.domain.performance.Performance;
 import com.thirdparty.ticketing.domain.zone.Zone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ZoneRepository extends JpaRepository<Zone, Long> {
+
+    List<Zone> findByPerformance(Performance performance);
 }
