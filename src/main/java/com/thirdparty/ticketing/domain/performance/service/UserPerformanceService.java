c68262fd85898cbddc3300e4b0ae287593117bed
lass9436 <lass9436@gmail.com>
2024-08-12 23:38:14 +0900

    feat: 유저 공연 서비스 작성 및 공연 조회 메서드 작성

---
+++ src/main/java/com/thirdparty/ticketing/domain/performance/service/UserPerformanceService.java
@@ -0,0 +1,22 @@
+package com.thirdparty.ticketing.domain.performance.service;
+
+import com.thirdparty.ticketing.domain.performance.dto.PerformanceElement;
+import com.thirdparty.ticketing.domain.performance.repository.PerformanceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserPerformanceService {
+
+    private final PerformanceRepository performanceRepository;
+
+    public List<PerformanceElement> getPerformances() {
+        return performanceRepository.findAll()
+                .stream()
+                .map(PerformanceElement::of)
+                .toList();
+    }
+}
