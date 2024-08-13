d96fbd7a65e2beb91f52160f7447c93be750401e
lass9436 <lass9436@gmail.com>
2024-08-13 11:18:15 +0900

    fix: 응답 DTO 수정
    PerformanceElement 와 ZoneElement 수정

--- src/main/java/com/thirdparty/ticketing/domain/performance/dto/PerformanceElement.java
+++ src/main/java/com/thirdparty/ticketing/domain/performance/dto/PerformanceElement.java
@@ -1,33 +1,27 @@
 package com.thirdparty.ticketing.domain.performance.dto;
 
-import com.thirdparty.ticketing.domain.performance.Performance;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-
 import java.time.ZonedDateTime;
+
+import com.thirdparty.ticketing.domain.performance.Performance;
+
+import lombok.Data;
 
 @Data
 public class PerformanceElement {
 
-    @NotBlank(message = "공연 아이디는 공백일 수 없습니다.")
-    private Long performanceId;
+	private final Long performanceId;
 
-    @NotBlank(message = "공연 이름은 공백일 수 없습니다.")
-    private String performanceName;
+	private final String performanceName;
 
-    @NotBlank(message = "공연 장소는 공백일 수 없습니다.")
-    private String performancePlace;
+	private final String performancePlace;
 
-    @NotNull(message = "공연 시간은 공백일 수 없습니다.")
-    private ZonedDateTime performanceShowtime;
+	private final ZonedDateTime performanceShowtime;
 
-    public static PerformanceElement of (Performance performance) {
-        PerformanceElement performanceElement = new PerformanceElement();
-        performanceElement.setPerformanceId(performance.getPerformanceId());
-        performanceElement.setPerformanceName(performance.getPerformanceName());
-        performanceElement.setPerformancePlace(performance.getPerformancePlace());
-        performanceElement.setPerformanceShowtime(performance.getPerformanceShowtime());
-        return performanceElement;
-    }
+	public static PerformanceElement of(Performance perf) {
+		return new PerformanceElement(
+			perf.getPerformanceId(),
+			perf.getPerformanceName(),
+			perf.getPerformancePlace(),
+			perf.getPerformanceShowtime());
+	}
 }
--- src/main/java/com/thirdparty/ticketing/domain/zone/dto/ZoneElement.java
+++ src/main/java/com/thirdparty/ticketing/domain/zone/dto/ZoneElement.java
@@ -1,22 +1,17 @@
 package com.thirdparty.ticketing.domain.zone.dto;
 
 import com.thirdparty.ticketing.domain.zone.Zone;
-import jakarta.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class ZoneElement {
 
-    @NotBlank(message = "구역 아이디는 공백일 수 없습니다.")
-    private Long zoneId;
+	private final Long zoneId;
 
-    @NotBlank(message = "구역이름은 공백일 수 없습니다.")
-    private String zoneName;
+	private final String zoneName;
 
-    public static ZoneElement of(Zone zone) {
-        ZoneElement zoneElement = new ZoneElement();
-        zoneElement.setZoneId(zone.getZoneId());
-        zoneElement.setZoneName(zone.getZoneName());
-        return zoneElement;
-    }
+	public static ZoneElement of(Zone zone) {
+		return new ZoneElement(zone.getZoneId(), zone.getZoneName());
+	}
 }
