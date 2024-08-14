4a72faf011dfe217e0b5692aac9a438589c08fc8
seminchoi <choicco89@gmail.com>
2024-08-14 15:51:59 +0900

    feat: 유효성 검증 실패 예외처리 및 응답 구현

--- src/main/java/com/thirdparty/ticketing/domain/common/GlobalExceptionHandler.java
+++ src/main/java/com/thirdparty/ticketing/domain/common/GlobalExceptionHandler.java
@@ -1,15 +1,25 @@
 package com.thirdparty.ticketing.domain.common;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(TicketingException.class)
-    public ResponseEntity<ErrorResponse> handleTicketingException(TicketingException e) {
+    public ResponseEntity<ErrorResponse<Void>> handleTicketingException(TicketingException e) {
         ErrorCode errorCode = e.getErrorCode();
         return ResponseEntity.status(errorCode.getHttpStatusValue())
                 .body(ErrorResponse.of(errorCode));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse<List<ValidationErrorDetail>>>
+            handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ErrorResponse<List<ValidationErrorDetail>> errorResponse = ErrorResponse.of(e);
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 }
