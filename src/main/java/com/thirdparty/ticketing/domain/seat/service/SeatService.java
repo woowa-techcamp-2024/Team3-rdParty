d1c6a44cdec34e996883e13fff82144692d22533
seminchoi <choicco89@gmail.com>
2024-08-14 10:49:56 +0900

    feat: 좌석 등급 목록 조회 API 구현

--- src/main/java/com/thirdparty/ticketing/domain/seat/service/SeatService.java
+++ src/main/java/com/thirdparty/ticketing/domain/seat/service/SeatService.java
@@ -7,7 +7,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.thirdparty.ticketing.domain.ItemResult;
 import com.thirdparty.ticketing.domain.common.TicketingException;
+import com.thirdparty.ticketing.domain.performance.Performance;
+import com.thirdparty.ticketing.domain.performance.repository.PerformanceRepository;
 import com.thirdparty.ticketing.domain.seat.dto.response.SeatElement;
+import com.thirdparty.ticketing.domain.seat.dto.response.SeatGradeElement;
+import com.thirdparty.ticketing.domain.seat.repository.SeatGradeRepository;
 import com.thirdparty.ticketing.domain.seat.repository.SeatRepository;
 import com.thirdparty.ticketing.domain.zone.Zone;
 import com.thirdparty.ticketing.domain.zone.repository.ZoneRepository;
@@ -17,7 +21,9 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class SeatService {
+    private final PerformanceRepository performanceRepository;
     private final ZoneRepository zoneRepository;
+    private final SeatGradeRepository seatGradeRepository;
     private final SeatRepository seatRepository;
 
     @Transactional(readOnly = true)
@@ -27,5 +33,19 @@ public class SeatService {
                 seatRepository.findByZone(zone).stream().map(SeatElement::of).toList();
 
         return ItemResult.of(seats);
+    }
+
+    @Transactional(readOnly = true)
+    public ItemResult<SeatGradeElement> getSeatGrades(Long performanceId) {
+        Performance performance =
+                performanceRepository
+                        .findById(performanceId)
+                        .orElseThrow(() -> new TicketingException(("")));
+        List<SeatGradeElement> seatGrades =
+                seatGradeRepository.findAllByPerformance(performance).stream()
+                        .map(SeatGradeElement::of)
+                        .toList();
+
+        return ItemResult.of(seatGrades);
     }
 }
