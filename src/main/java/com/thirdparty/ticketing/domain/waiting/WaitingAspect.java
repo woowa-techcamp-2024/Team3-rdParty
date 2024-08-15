72fe707da6a3868fa302d9899730cd20246e6a18
lass9436 <lass9436@gmail.com>
2024-08-15 22:10:57 +0900

    refactor: spotless 적용

--- src/main/java/com/thirdparty/ticketing/domain/waiting/WaitingAspect.java
+++ src/main/java/com/thirdparty/ticketing/domain/waiting/WaitingAspect.java
@@ -1,16 +1,18 @@
 package com.thirdparty.ticketing.domain.waiting;
 
-import com.thirdparty.ticketing.domain.waiting.manager.WaitingManager;
 import jakarta.servlet.http.HttpServletRequest;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.thirdparty.ticketing.domain.waiting.manager.WaitingManager;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class WaitingAspect {
@@ -20,7 +22,9 @@ public class WaitingAspect {
     private Object waitingRequest(ProceedingJoinPoint joinPoint) throws Throwable {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String email = (String) authentication.getPrincipal();
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        HttpServletRequest request =
+                ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+                        .getRequest();
         Long performanceId = Long.valueOf(request.getHeader("performanceId"));
         WaitingMember waitingMember = new WaitingMember(email, performanceId);
         if (waitingManager.isReadyToHandle(waitingMember)) {
