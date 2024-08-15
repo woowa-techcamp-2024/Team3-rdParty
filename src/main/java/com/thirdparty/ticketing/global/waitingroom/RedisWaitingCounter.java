b94ca9b4d79c34afa97965656bf86f60fe11b86b
hseong3243 <hseong3243@gmail.com>
2024-08-15 10:58:20 +0900

    feat: 고유한 대기 순번 카운터 구현
    레디스를 이용해 각 공연마다 사용자에게 고유한 대기 순번을 발급해주는 카운터를 구현함.

---
+++ src/main/java/com/thirdparty/ticketing/global/waitingroom/RedisWaitingCounter.java
@@ -0,0 +1,23 @@
+package com.thirdparty.ticketing.global.waitingroom;
+
+import com.thirdparty.ticketing.domain.waitingroom.WaitingCounter;
+import com.thirdparty.ticketing.domain.waitingroom.WaitingMember;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+public class RedisWaitingCounter implements WaitingCounter {
+
+    private static final String WAITING_COUNTER_KEY = "waiting_counter";
+
+    private final ValueOperations<String, String> counter;
+
+    public RedisWaitingCounter(RedisTemplate<String, String> redisTemplate) {
+        this.counter = redisTemplate.opsForValue();
+    }
+
+    @Override
+    public long getNextCount(WaitingMember waitingMember) {
+        String performanceWaitingCounterKey = WAITING_COUNTER_KEY + waitingMember.getPerformanceId();
+        return counter.increment(performanceWaitingCounterKey, 1);
+    }
+}
