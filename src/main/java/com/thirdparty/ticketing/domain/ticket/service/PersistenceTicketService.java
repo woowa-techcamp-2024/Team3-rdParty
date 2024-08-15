373ced932256cd00f5b0320d71c608bd6bbc2414
seminchoi <choicco89@gmail.com>
2024-08-15 21:28:27 +0900

    feat: 티켓 예매시 데이터 정합성이 유지되도록 DB Locking 적용

--- src/main/java/com/thirdparty/ticketing/domain/ticket/service/PersistenceTicketService.java
+++ src/main/java/com/thirdparty/ticketing/domain/ticket/service/PersistenceTicketService.java
@@ -1,75 +1,83 @@
 package com.thirdparty.ticketing.domain.ticket.service;
 
 import jakarta.persistence.OptimisticLockException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.thirdparty.ticketing.domain.common.ErrorCode;
 import com.thirdparty.ticketing.domain.common.TicketingException;
 import com.thirdparty.ticketing.domain.member.Member;
 import com.thirdparty.ticketing.domain.member.repository.MemberRepository;
-import com.thirdparty.ticketing.domain.seat.Seat;
 import com.thirdparty.ticketing.domain.payment.PaymentProcessor;
+import com.thirdparty.ticketing.domain.payment.dto.PaymentRequest;
+import com.thirdparty.ticketing.domain.seat.Seat;
+import com.thirdparty.ticketing.domain.seat.SeatStatus;
 import com.thirdparty.ticketing.domain.seat.repository.SeatRepository;
 import com.thirdparty.ticketing.domain.ticket.dto.SeatSelectionRequest;
 import com.thirdparty.ticketing.domain.ticket.dto.TicketPaymentRequest;
+import com.thirdparty.ticketing.domain.ticket.policy.LockSeatStrategy;
 import com.thirdparty.ticketing.domain.ticket.repository.TicketRepository;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-import java.util.logging.Logger;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class PersistenceTicketService extends TicketService {
-    private static final Logger log = LoggerFactory.getLogger(PersistenceTicketService.class);
+    private final LockSeatStrategy lockSeatStrategy;
 
     public PersistenceTicketService(
             MemberRepository memberRepository,
             TicketRepository ticketRepository,
             SeatRepository seatRepository,
-            PaymentProcessor paymentProcessor) {
+            PaymentProcessor paymentProcessor,
+            LockSeatStrategy lockSeatStrategy) {
         super(memberRepository, ticketRepository, seatRepository, paymentProcessor);
+        this.lockSeatStrategy = lockSeatStrategy;
     }
 
     @Override
     @Transactional
-    public void selectSeat(SeatSelectionRequest seatSelectionRequest) {
+    public void selectSeat(String memberEmail, SeatSelectionRequest seatSelectionRequest) {
         try {
             Long seatId = seatSelectionRequest.getSeatId();
-            Optional<Seat> byId = seatRepository.findById(seatId);
-            Seat seat = byId.orElseThrow(() -> new TicketingException(ErrorCode.NOT_FOUND_SEAT));
-
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            String email = (String) authentication.getPrincipal();
+            Seat seat =
+                    lockSeatStrategy
+                            .getSeatWithLock(seatId)
+                            .orElseThrow(() -> new TicketingException(ErrorCode.NOT_FOUND_SEAT));
 
             Member member =
                     memberRepository
-                            .findByEmail(email)
+                            .findByEmail(memberEmail)
                             .orElseThrow(() -> new TicketingException(ErrorCode.NOT_FOUND_MEMBER));
 
             seat.assignByMember(member);
         } catch (OptimisticLockException e) {
-            log.error("optimistic lock exception", e);
+            throw new TicketingException(ErrorCode.NOT_SELECTABLE_SEAT);
         }
     }
 
     @Override
     @Transactional
-    public void reservationTicket(TicketPaymentRequest ticketPaymentRequest) {
+    public void reservationTicket(String memberEmail, TicketPaymentRequest ticketPaymentRequest) {
         Long seatId = ticketPaymentRequest.getSeatId();
         Seat seat =
-                seatRepository
-                        .findById(seatId)
+                lockSeatStrategy
+                        .getSeatWithLock(seatId)
                         .orElseThrow(() -> new TicketingException(ErrorCode.NOT_FOUND_SEAT));
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String email = (String) authentication.getPrincipal();
 
         Member loginMember =
                 memberRepository
-                        .findByEmail(email)
+                        .findByEmail(memberEmail)
                         .orElseThrow(() -> new TicketingException(ErrorCode.NOT_FOUND_MEMBER));
+
+        if (!seat.getSeatStatus().equals(SeatStatus.SELECTED)) {
+            seat.updateStatus(SeatStatus.PENDING_PAYMENT);
+        }
+        paymentProcessor.processPayment(new PaymentRequest());
+        if (!seat.getSeatStatus().equals(SeatStatus.PAID)) {
+            seat.updateStatus(SeatStatus.PAID);
+        }
 
         if (!seat.getMember().getMemberId().equals(loginMember.getMemberId())) {
             throw new TicketingException(ErrorCode.NOT_SELECTABLE_SEAT);
