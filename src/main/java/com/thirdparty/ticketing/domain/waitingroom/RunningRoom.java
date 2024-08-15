2ffd84b972cf56f2b2de5467016484622d1dac13
lass9436 <lass9436@gmail.com>
2024-08-15 18:03:22 +0900

    feat: WaitingMember 작성
    UserInfo 에서 WaitingMember 로 수정하고 필드 추가

--- src/main/java/com/thirdparty/ticketing/domain/waitingroom/RunningRoom.java
+++ src/main/java/com/thirdparty/ticketing/domain/waitingroom/RunningRoom.java
@@ -1,5 +1,5 @@
 package com.thirdparty.ticketing.domain.waitingroom;
 
 public interface RunningRoom {
-    boolean contains(UserInfo userInfo);
+    boolean contains(WaitingMember waitingMember);
 }
