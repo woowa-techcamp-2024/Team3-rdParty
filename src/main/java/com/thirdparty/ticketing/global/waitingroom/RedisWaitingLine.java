4e53853b85439ff8adb476a58c6dc1f6659f52fb
hseong3243 <hseong3243@gmail.com>
2024-08-15 12:02:22 +0900

    feat: 대기열 구현
    레디스의 sortedSet 자료 구조를 이용하여 대기열을 구현함.
    sortedSet의 score로 대기 순번을 이용함.

---
+++ src/main/java/com/thirdparty/ticketing/global/waitingroom/RedisWaitingLine.java
@@ -0,0 +1,35 @@
+package com.thirdparty.ticketing.global.waitingroom;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thirdparty.ticketing.domain.common.TicketingException;
+import com.thirdparty.ticketing.domain.waitingroom.WaitingLine;
+import com.thirdparty.ticketing.domain.waitingroom.WaitingMember;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+
+public class RedisWaitingLine implements WaitingLine {
+
+    private static final String WAITING_LINE_KEY = "waiting_line:";
+
+    private final ObjectMapper objectMapper;
+    private final ZSetOperations<String, String> waitingLine;
+
+    public RedisWaitingLine(ObjectMapper objectMapper, RedisTemplate<String, String> template) {
+        this.objectMapper = objectMapper;
+        this.waitingLine = template.opsForZSet();
+    }
+
+    @Override
+    public void enter(WaitingMember waitingMember, long waitingCounter) {
+        String performanceWaitingLineKey = WAITING_LINE_KEY + waitingMember.getPerformanceId();
+        String waitingMemberValue;
+        try {
+            waitingMemberValue = objectMapper.writeValueAsString(waitingMember);
+        } catch (JsonProcessingException e) {
+            throw new TicketingException("json 직렬화 예외 발생");
+        }
+        waitingLine.add(performanceWaitingLineKey, waitingMemberValue, waitingCounter);
+    }
+
+}
