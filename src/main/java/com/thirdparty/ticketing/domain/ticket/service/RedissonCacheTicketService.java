fc6a22919a8a074a83219fc153e0a2bd9fadc20f
mirageoasis <kimhw0820@naver.com>
2024-08-16 00:12:03 +0900

    feature: 티켓 구매 기능 임시구현

--- src/main/java/com/thirdparty/ticketing/domain/ticket/service/RedissonCacheTicketService.java
+++ src/main/java/com/thirdparty/ticketing/domain/ticket/service/RedissonCacheTicketService.java
@@ -1,13 +1,17 @@
 package com.thirdparty.ticketing.domain.ticket.service;
 
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
+
 
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.thirdparty.ticketing.domain.member.repository.MemberRepository;
 import com.thirdparty.ticketing.domain.payment.PaymentProcessor;
+import com.thirdparty.ticketing.domain.seat.Seat;
 import com.thirdparty.ticketing.domain.seat.repository.SeatRepository;
 import com.thirdparty.ticketing.domain.ticket.dto.SeatSelectionRequest;
 import com.thirdparty.ticketing.domain.ticket.dto.TicketPaymentRequest;
@@ -50,5 +54,21 @@ public class RedissonCacheTicketService extends TicketService {
     }
 
     @Override
-    public void reservationTicket(String memberEmail, TicketPaymentRequest ticketPaymentRequest) {}
+    @Transactional
+    public void reservationTicket(String memberEmail, TicketPaymentRequest ticketPaymentRequest) {
+        RLock lock = redissonClient.getLock(ticketPaymentRequest.getSeatId().toString());
+
+        try {
+            boolean available = lock.tryLock(5, 300, TimeUnit.MICROSECONDS);
+            if (!available) {
+                return;
+            }
+
+            cacheTicketService.reservationTicket(memberEmail, ticketPaymentRequest);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
 }
