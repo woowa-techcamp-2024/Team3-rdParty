159edcd58bef29dc5c7d12a5bdf7521d74ed9cb1
hseong3243 <hseong3243@gmail.com>
2024-08-16 23:43:08 +0900

    test: 사용자의 작업 가능 여부 레디스 조회 기능 검증

---
+++ src/test/java/com/thirdparty/ticketing/global/waitingsystem/TestRedisConfig.java
@@ -0,0 +1,20 @@
+package com.thirdparty.ticketing.global.waitingsystem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@TestConfiguration
+public class TestRedisConfig {
+
+    @Qualifier("lettuceRedisTemplate")
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Bean
+    public RedisRunningRoom runningRoom() {
+        return new RedisRunningRoom(redisTemplate);
+    }
+}
