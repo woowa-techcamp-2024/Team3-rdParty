0e24fab2acb84feabd09d4312b88a9aa58fcfe00
lass9436 <lass9436@gmail.com>
2024-08-17 17:38:37 +0900

    test: 사용자의 작업 여부를 확인하는 메서드의 테스트를 작성

---
+++ src/test/java/com/thirdparty/ticketing/global/waitingsystem/memory/running/MemoryRunningRoomTest.java
@@ -0,0 +1,78 @@
+package com.thirdparty.ticketing.global.waitingsystem.memory.running;
+
+import com.thirdparty.ticketing.domain.waitingsystem.waiting.WaitingMember;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemoryRunningRoomTest {
+
+    private MemoryRunningRoom runningRoom;
+
+    @Nested
+    @DisplayName("러닝 룸에 사용자가 있는지 확인했을 때")
+    class ContainTest {
+
+        private Map<Long, Map<String, WaitingMember>> map;
+
+        @BeforeEach
+        void setUp() {
+            map = new HashMap<>();
+            runningRoom = new MemoryRunningRoom(map);
+        }
+
+        @Test
+        @DisplayName("사용자가 포함되어 있다면 true 를 반환한다.")
+        void true_WhenMemberContains() {
+            // given
+            long performanceId = 1;
+            String email = "email@email.com";
+            map.putIfAbsent(performanceId, new ConcurrentHashMap<>());
+            map.get(performanceId).putIfAbsent(email, new WaitingMember());
+
+            // when
+            boolean contains = runningRoom.contains(email, performanceId);
+
+            // then
+            assertThat(contains).isTrue();
+        }
+
+        @Test
+        @DisplayName("사용자가 포함되어 있지 않다면 false 를 반환한다.")
+        void false_WhenMemberDoesNotContains() {
+            // given
+            long performanceId = 1;
+            String email = "email@email.com";
+
+            // when
+            boolean contains = runningRoom.contains(email, performanceId);
+
+            // then
+            assertThat(contains).isFalse();
+        }
+
+        @Test
+        @DisplayName("서로 다른 공연은 러닝룸을 공유하지 않는다.")
+        void doesNotShareRunningRoom_BetweenPerformances() {
+            // given
+            long performanceIdA = 1;
+            long performanceIdB = 2;
+            String email = "email@email.com";
+            map.putIfAbsent(performanceIdA, new ConcurrentHashMap<>());
+            map.get(performanceIdA).putIfAbsent(email, new WaitingMember());
+
+            // when
+            boolean contains = runningRoom.contains(email, performanceIdB);
+
+            // then
+            assertThat(contains).isFalse();
+        }
+    }
+}
