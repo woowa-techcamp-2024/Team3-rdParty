c60b8324b6c5273ce9b450e37b293c87cfb13ee1
seminchoi <choicco89@gmail.com>
2024-08-17 13:57:48 +0900

    feat: 서비스 계층 구조 변경 완료에 따라 컨트롤러 기능 재구현

--- src/main/java/com/thirdparty/ticketing/domain/ticket/controller/TicketController.java
+++ src/main/java/com/thirdparty/ticketing/domain/ticket/controller/TicketController.java
@@ -4,26 +4,37 @@ import java.util.List;
 
 import jakarta.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.thirdparty.ticketing.domain.ItemResult;
 import com.thirdparty.ticketing.domain.common.LoginMember;
 import com.thirdparty.ticketing.domain.ticket.dto.SeatSelectionRequest;
 import com.thirdparty.ticketing.domain.ticket.dto.TicketElement;
 import com.thirdparty.ticketing.domain.ticket.dto.TicketPaymentRequest;
+import com.thirdparty.ticketing.domain.ticket.service.ReservationServiceProxy;
+import com.thirdparty.ticketing.domain.ticket.service.TicketService;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class TicketController {
+    private final TicketService ticketService;
+    private final ReservationServiceProxy reservationServiceProxy;
+
+    public TicketController(
+            TicketService ticketService,
+            @Qualifier("reddisonReservationServiceProxy")
+                    ReservationServiceProxy reservationServiceProxy) {
+        this.ticketService = ticketService;
+        this.reservationServiceProxy = reservationServiceProxy;
+    }
 
     @GetMapping("/members/tickets")
     public ResponseEntity<ItemResult<TicketElement>> selectMyTickets(
             @LoginMember String memberEmail) {
         ItemResult<TicketElement> tickets = ItemResult.of(List.of());
-        // ticketService.selectMyTicket(memberEmail);
+        ticketService.selectMyTicket(memberEmail);
         return ResponseEntity.ok().body(tickets);
     }
 
@@ -31,13 +42,15 @@ public class TicketController {
     public ResponseEntity<Void> selectSeat(
             @LoginMember String memberEmail,
             @RequestBody @Valid SeatSelectionRequest seatSelectionRequest) {
+        reservationServiceProxy.selectSeat(memberEmail, seatSelectionRequest);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/tickets")
-    public ResponseEntity<Void> payTicket(
+    public ResponseEntity<Void> reservationTicket(
             @LoginMember String memberEmail,
             @RequestBody @Valid TicketPaymentRequest ticketPaymentRequest) {
+        reservationServiceProxy.reservationTicket(memberEmail, ticketPaymentRequest);
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/thirdparty/ticketing/domain/ticket/service/TicketService.java
+++ src/main/java/com/thirdparty/ticketing/domain/ticket/service/TicketService.java
@@ -2,24 +2,23 @@ package com.thirdparty.ticketing.domain.ticket.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.thirdparty.ticketing.domain.ItemResult;
 import com.thirdparty.ticketing.domain.common.ErrorCode;
 import com.thirdparty.ticketing.domain.common.TicketingException;
 import com.thirdparty.ticketing.domain.member.Member;
 import com.thirdparty.ticketing.domain.member.repository.MemberRepository;
-import com.thirdparty.ticketing.domain.payment.PaymentProcessor;
-import com.thirdparty.ticketing.domain.seat.repository.SeatRepository;
 import com.thirdparty.ticketing.domain.ticket.dto.TicketElement;
 import com.thirdparty.ticketing.domain.ticket.repository.TicketRepository;
 
 import lombok.RequiredArgsConstructor;
 
+@Service
 @RequiredArgsConstructor
 public class TicketService {
     protected final MemberRepository memberRepository;
     protected final TicketRepository ticketRepository;
-    protected final SeatRepository seatRepository;
-    protected final PaymentProcessor paymentProcessor;
 
     public ItemResult<TicketElement> selectMyTicket(String memberEmail) {
         Member member =
