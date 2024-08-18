53ff47f95a78520a339e193594a445bad24536aa
hseong3243 <hseong3243@gmail.com>
2024-08-18 14:01:27 +0900

    refactor: 코드 포맷 정렬

--- src/test/java/com/thirdparty/ticketing/global/waitingsystem/redis/running/RedisRunningCounterTest.java
+++ src/test/java/com/thirdparty/ticketing/global/waitingsystem/redis/running/RedisRunningCounterTest.java
@@ -2,12 +2,11 @@ package com.thirdparty.ticketing.global.waitingsystem.redis.running;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.thirdparty.ticketing.global.waitingsystem.redis.TestRedisConfig;
-import com.thirdparty.ticketing.support.TestContainerStarter;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -19,15 +18,16 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
+import com.thirdparty.ticketing.global.waitingsystem.redis.TestRedisConfig;
+import com.thirdparty.ticketing.support.TestContainerStarter;
+
 @SpringBootTest
 @Import(TestRedisConfig.class)
 class RedisRunningCounterTest extends TestContainerStarter {
 
-    @Autowired
-    private RedisRunningCounter runningCounter;
+    @Autowired private RedisRunningCounter runningCounter;
 
-    @Autowired
-    private StringRedisTemplate redisTemplate;
+    @Autowired private StringRedisTemplate redisTemplate;
 
     private ValueOperations<String, String> rawRunningCounter;
 
@@ -48,43 +48,45 @@ class RedisRunningCounterTest extends TestContainerStarter {
         @Test
         @DisplayName("카운터가 초기화되지 않았다면 0으로 초기화한다.")
         void initializeCounter() {
-            //given
+            // given
             long performanceId = 1;
 
-            //when
+            // when
             long runningCount = runningCounter.getRunningCount(performanceId);
 
-            //then
+            // then
             assertThat(runningCount).isEqualTo(0);
         }
 
         @RepeatedTest(5)
         @DisplayName("카운터가 초기화되었다면 0으로 초기화하지 않는다.")
         void doNotReInitializeCounter() throws InterruptedException, ExecutionException {
-            //given
+            // given
             long performanceId = 1;
             int poolSize = 50;
             ExecutorService executorService = Executors.newFixedThreadPool(poolSize);
             CountDownLatch latch = new CountDownLatch(poolSize);
 
-            //when
-            for(int i=0; i<poolSize; i++) {
+            // when
+            for (int i = 0; i < poolSize; i++) {
                 int finalI = i;
-                executorService.execute(() -> {
-                    try {
-                        if (finalI % 2 == 0) {
-                            rawRunningCounter.set(getRunningCounterKey(performanceId), "10");
-                        } else {
-                            runningCounter.getRunningCount(performanceId);
-                        }
-                    } finally {
-                        latch.countDown();
-                    }
-                });
+                executorService.execute(
+                        () -> {
+                            try {
+                                if (finalI % 2 == 0) {
+                                    rawRunningCounter.set(
+                                            getRunningCounterKey(performanceId), "10");
+                                } else {
+                                    runningCounter.getRunningCount(performanceId);
+                                }
+                            } finally {
+                                latch.countDown();
+                            }
+                        });
             }
             latch.await();
 
-            //then
+            // then
             long runningCount = runningCounter.getRunningCount(performanceId);
             assertThat(runningCount).isEqualTo(10);
         }
