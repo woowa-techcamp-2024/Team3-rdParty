ee4146714e8166d7bc693a2474cfcdef76a7e6ef
hseong3243 <hseong3243@gmail.com>
2024-08-18 14:41:15 +0900

    test: 작업 가능 인원 수 조회 로직 검증

--- src/test/java/com/thirdparty/ticketing/global/waitingsystem/redis/running/RedisRunningRoomTest.java
+++ src/test/java/com/thirdparty/ticketing/global/waitingsystem/redis/running/RedisRunningRoomTest.java
@@ -2,18 +2,19 @@ package com.thirdparty.ticketing.global.waitingsystem.redis.running;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.thirdparty.ticketing.global.waitingsystem.redis.TestRedisConfig;
+import com.thirdparty.ticketing.support.TestContainerStarter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
-
-import com.thirdparty.ticketing.global.waitingsystem.redis.TestRedisConfig;
-import com.thirdparty.ticketing.support.TestContainerStarter;
 
 @SpringBootTest
 @Import(TestRedisConfig.class)
@@ -23,8 +24,11 @@ class RedisRunningRoomTest extends TestContainerStarter {
 
     @Autowired private StringRedisTemplate redisTemplate;
 
+    private SetOperations<String, String> rawRunningRoom;
+
     @BeforeEach
     void setUp() {
+        rawRunningRoom = redisTemplate.opsForSet();
         redisTemplate.getConnectionFactory().getConnection().serverCommands().flushAll();
     }
 
@@ -35,13 +39,6 @@ class RedisRunningRoomTest extends TestContainerStarter {
     @Nested
     @DisplayName("러닝룸에 사용자가 있는지 확인했을 때")
     class ContainsTest {
-
-        private SetOperations<String, String> rawRunningRoom;
-
-        @BeforeEach
-        void setUp() {
-            rawRunningRoom = redisTemplate.opsForSet();
-        }
 
         @Test
         @DisplayName("사용자가 포함되어 있다면 true를 반환한다.")
@@ -86,6 +83,32 @@ class RedisRunningRoomTest extends TestContainerStarter {
 
             // then
             assertThat(contains).isFalse();
+        }
+    }
+
+    @Nested
+    @DisplayName("작업 가능 공간에 빈 자리가 있는지 조회 시")
+    class GetAvailableToRunningTest {
+
+        @ParameterizedTest
+        @CsvSource({
+                "0, 100",
+                "50, 50",
+                "100, 0"
+        })
+        @DisplayName("남아있는 자리를 반환한다.")
+        void getAvailableToRunning(int runningMembers, int expectedAvailableToRunning) {
+            //given
+            long performanceId = 1;
+            for(int i=0; i<runningMembers; i++) {
+                rawRunningRoom.add(getRunningRoomKey(performanceId), "email" + i + "@email.com");
+            }
+
+            //when
+            long availableToRunning = runningRoom.getAvailableToRunning(performanceId);
+
+            //then
+            assertThat(availableToRunning).isEqualTo(expectedAvailableToRunning);
         }
     }
 }
