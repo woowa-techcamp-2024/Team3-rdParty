dddcb93940a26e59000ccc025beab9d9797e25f7
hseong3243 <hseong3243@gmail.com>
2024-08-19 15:52:33 +0900

    setting: 대기열 시스템 빈 등록
    대기열 시스템을 구성하는 객체를 빈으로 등록함.

---
+++ src/main/java/com/thirdparty/ticketing/global/config/EventConfig.java
@@ -0,0 +1,23 @@
+package com.thirdparty.ticketing.global.config;
+
+import com.thirdparty.ticketing.domain.common.EventPublisher;
+import com.thirdparty.ticketing.domain.waitingsystem.WaitingSystem;
+import com.thirdparty.ticketing.global.event.SpringEventPublisher;
+import com.thirdparty.ticketing.global.waitingsystem.WaitingEventListener;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EventConfig {
+
+    @Bean
+    public EventPublisher eventPublisher(ApplicationEventPublisher eventPublisher) {
+        return new SpringEventPublisher(eventPublisher);
+    }
+
+    @Bean
+    public WaitingEventListener waitingEventListener(WaitingSystem waitingSystem) {
+        return new WaitingEventListener(waitingSystem);
+    }
+}
---
+++ src/main/java/com/thirdparty/ticketing/global/config/WaitingConfig.java
@@ -0,0 +1,74 @@
+package com.thirdparty.ticketing.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thirdparty.ticketing.domain.common.EventPublisher;
+import com.thirdparty.ticketing.domain.waitingsystem.WaitingSystem;
+import com.thirdparty.ticketing.domain.waitingsystem.running.RunningManager;
+import com.thirdparty.ticketing.domain.waitingsystem.waiting.WaitingManager;
+import com.thirdparty.ticketing.global.waitingsystem.redis.running.RedisRunningCounter;
+import com.thirdparty.ticketing.global.waitingsystem.redis.running.RedisRunningManager;
+import com.thirdparty.ticketing.global.waitingsystem.redis.running.RedisRunningRoom;
+import com.thirdparty.ticketing.global.waitingsystem.redis.waiting.RedisWaitingCounter;
+import com.thirdparty.ticketing.global.waitingsystem.redis.waiting.RedisWaitingLine;
+import com.thirdparty.ticketing.global.waitingsystem.redis.waiting.RedisWaitingManager;
+import com.thirdparty.ticketing.global.waitingsystem.redis.waiting.RedisWaitingRoom;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+public class WaitingConfig {
+
+    @Bean
+    public WaitingSystem waitingSystem(
+            WaitingManager waitingManager,
+            RunningManager runningManager,
+            EventPublisher eventPublisher) {
+        return new WaitingSystem(waitingManager, runningManager, eventPublisher);
+    }
+
+    @Bean
+    public WaitingManager waitingManager(
+            RedisWaitingRoom waitingRoom,
+            RedisWaitingLine waitingLine,
+            RedisWaitingCounter waitingCounter
+    ) {
+        return new RedisWaitingManager(waitingRoom, waitingCounter, waitingLine);
+    }
+
+    @Bean
+    public RedisWaitingRoom waitingRoom(
+            StringRedisTemplate redisTemplate,
+            ObjectMapper objectMapper) {
+        return new RedisWaitingRoom(redisTemplate, objectMapper);
+    }
+
+    @Bean
+    public RedisWaitingLine waitingLine(
+            StringRedisTemplate redisTemplate,
+            ObjectMapper objectMapper) {
+        return new RedisWaitingLine(redisTemplate, objectMapper);
+    }
+
+    @Bean
+    public RedisWaitingCounter waitingCounter(StringRedisTemplate redisTemplate) {
+        return new RedisWaitingCounter(redisTemplate);
+    }
+
+    @Bean
+    public RunningManager runningManager(
+            RedisRunningRoom runningRoom,
+            RedisRunningCounter runningCounter) {
+        return new RedisRunningManager(runningRoom, runningCounter);
+    }
+
+    @Bean
+    public RedisRunningRoom runningRoom(StringRedisTemplate redisTemplate) {
+        return new RedisRunningRoom(redisTemplate);
+    }
+
+    @Bean
+    public RedisRunningCounter runningCounter(StringRedisTemplate redisTemplate) {
+        return new RedisRunningCounter(redisTemplate);
+    }
+}
