70d5f61c434b43a0d7dacb480969f362db951850
mirageoasis <kimhw0820@naver.com>
2024-08-20 21:15:03 +0900

    chore: 로컬 환경 설정

---
+++ scripts/local-end.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 현재 스크립트의 디렉토리로 이동
+cd "$(dirname "$0")"
+
+# Docker Compose 파일이 있는 디렉토리로 이동
+cd ../docker
+
+# Docker Compose를 사용하여 서비스 종료
+docker-compose -f docker-compose.yml down
