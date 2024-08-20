1c8d7736b4c3b9d784292fdde0b606464dbeccfe
hseong3243 <hseong3243@gmail.com>
2024-08-20 20:01:16 +0900

    refactor: 레디스 config 클래스 프로파일 분리

--- src/main/java/com/thirdparty/ticketing/global/config/LocalRedisConfig.java
+++ src/main/java/com/thirdparty/ticketing/global/config/LocalRedisConfig.java
@@ -1,20 +1,46 @@
 package com.thirdparty.ticketing.global.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@Profile({"local", "default"})
 @Configuration
-public class LettuceConfig {
+public class LocalRedisConfig {
 
-    @Bean
-    public LettuceConnectionFactory lettuceConnectionFactory(
+    private final int port;
+    private final String host;
+
+    public LocalRedisConfig(
             @Value("${spring.data.redis.port}") int port,
             @Value("${spring.data.redis.host}") String host) {
-        return new LettuceConnectionFactory(host, port);
+        this.port = port;
+        this.host = host;
+    }
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        SingleServerConfig serverConfig = config.useSingleServer();
+        serverConfig.setAddress("redis://" + host + ":" + port);
+        return Redisson.create(config);
+    }
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
