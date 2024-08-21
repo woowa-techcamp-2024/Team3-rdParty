10a26402e77ee98972f80a0184e690f55e405be5
hseong3243 <hseong3243@gmail.com>
2024-08-21 16:02:17 +0900

    test: MySQL 테스트 컨테이너 설정 제거
    히카리풀 커넥션 검증으로 인해 테스트 종료가 지연되는 문제가 있어 제거함.

--- src/test/java/com/thirdparty/ticketing/support/TestContainerStarter.java
+++ src/test/java/com/thirdparty/ticketing/support/TestContainerStarter.java
@@ -3,7 +3,6 @@ package com.thirdparty.ticketing.support;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
@@ -13,36 +12,14 @@ public class TestContainerStarter {
     private static final int REDIS_PORT = 6379;
     private static final GenericContainer<?> REDIS;
 
-    private static final String MYSQL_IMAGE = "mysql:8.0.32";
-    private static final int MYSQL_PORT = 3306;
-    private static final MySQLContainer<?> MYSQL;
-
     static {
         REDIS = new GenericContainer<>(REDIS_IMAGE).withExposedPorts(REDIS_PORT).withReuse(true);
         REDIS.start();
-
-        MYSQL =
-                new MySQLContainer<>(MYSQL_IMAGE)
-                        .withExposedPorts(MYSQL_PORT)
-                        .withUsername("root")
-                        .withPassword("root")
-                        .withDatabaseName("ticketing")
-                        .withReuse(false);
-        MYSQL.start();
     }
 
     @DynamicPropertySource
     private static void registerProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.data.redis.host", REDIS::getHost);
         registry.add("spring.data.redis.port", () -> REDIS.getMappedPort(REDIS_PORT).toString());
-
-        String mysqlUrl =
-                String.format(
-                        "jdbc:mysql://%s:%s/ticketing",
-                        MYSQL.getHost(), MYSQL.getMappedPort(MYSQL_PORT));
-
-        registry.add("spring.datasource.url", () -> mysqlUrl);
-        registry.add("spring.datasource.username", () -> "root");
-        registry.add("spring.datasource.password", () -> "root");
     }
 }
