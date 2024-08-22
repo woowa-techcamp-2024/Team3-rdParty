2762bc05b9c9be4ad98ff3938da6eed3963fc968
lass9436 <lass9436@gmail.com>
2024-08-22 12:08:14 +0900

    refactor: 코드 포맷터 적용

--- src/main/java/com/thirdparty/ticketing/domain/seat/controller/SeatController.java
+++ src/main/java/com/thirdparty/ticketing/domain/seat/controller/SeatController.java
@@ -1,6 +1,5 @@
 package com.thirdparty.ticketing.domain.seat.controller;
 
-import com.thirdparty.ticketing.domain.waitingsystem.Waiting;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +10,7 @@ import com.thirdparty.ticketing.domain.ItemResult;
 import com.thirdparty.ticketing.domain.seat.dto.response.SeatElement;
 import com.thirdparty.ticketing.domain.seat.dto.response.SeatGradeElement;
 import com.thirdparty.ticketing.domain.seat.service.SeatService;
+import com.thirdparty.ticketing.domain.waitingsystem.Waiting;
 
 import lombok.RequiredArgsConstructor;
 
--- src/main/java/com/thirdparty/ticketing/domain/seat/service/SeatService.java
+++ src/main/java/com/thirdparty/ticketing/domain/seat/service/SeatService.java
@@ -60,5 +60,4 @@ public class SeatService {
                         .toList();
         return ItemResult.of(seats);
     }
-
 }
